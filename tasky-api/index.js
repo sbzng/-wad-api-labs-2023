0babd0eafcc5f7acc2ae3882370ecbd72e38d6aa
Shaobo Zang <20105527@mail.wit.ie>
2023-12-05 13:47:53 +0000

    Mongo lab Users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -20,6 +21,8 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use('/api/tasks', tasksRouter);
 
+//Users router
+app.use('/api/users', usersRouter);
 app.use(errHandler);
 
 app.listen(port, () => {
