1bc2daaf6c35524be1927b566be9ec8c9ec6682e
Shaobo Zang <20105527@mail.wit.ie>
2023-12-12 15:23:37 +0000

    Added week 11 exercise (genres)

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -2,7 +2,8 @@ import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
 import {
-    getUpcomingMovies
+    getUpcomingMovies,
+    getGenres
   } from '../tmdb-api';
 
 const router = express.Router();
@@ -42,6 +43,11 @@ router.get('/:id', asyncHandler(async (req, res) => {
 router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
     const upcomingMovies = await getUpcomingMovies();
     res.status(200).json(upcomingMovies);
+}));
+
+router.get('/tmdb/genres', asyncHandler(async (req, res) => {
+    const genres = await getGenres();
+    res.status(200).json(genres);
 }));
 
 export default router;
--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -15,3 +15,20 @@ export const getUpcomingMovies = async () => {
         throw error;
     }
 };
+
+export const getGenres = async () => {
+    try {
+        const response = await fetch(
+            `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.TMDB_KEY}&language=en-US`
+        );
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch genres');
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error('Error in getGenres:', error);
+        throw error;
+    }
+};
